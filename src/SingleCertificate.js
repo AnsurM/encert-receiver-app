6d6ca6d6c4038f71a3b7a10d8d7f66253f2da467
kashan siddiqui <kashanzahoorsiddiqui@gmail.com>
2019-04-19 18:17:55 +0500

    redux setup complete with cert data

--- src/SingleCertificate.js
+++ src/SingleCertificate.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import reducers from './redux/reducers';
 
 class SingleCertificate extends Component {
 
     state = {  };
 
     render() { 
+        console.log(this.props.certificateData,"state");
         return (
             <div>
                 <h1>Certificate here</h1>
@@ -13,4 +16,12 @@ class SingleCertificate extends Component {
     }
 
 }
-export default SingleCertificate;
+function mapStateToProp(state) {
+    console.log(state)
+    return ({
+        certificateData: state.signIn_reducer.certificate_data,
+    })
+}
+
+export default connect(mapStateToProp,null)(SingleCertificate);
+// export default SingleCertificate;
