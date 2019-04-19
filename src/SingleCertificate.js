4d234c7d06e3b1c7b7a7c7da98c37c3e7e092a99
AnsurM <ansurmehdi@gmail.com>
2019-04-19 16:44:07 +0500

    Added single certificate route (without data display redux)

---
+++ src/SingleCertificate.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SingleCertificate extends Component {
+
+    state = {  };
+
+    render() { 
+        return (
+            <div>
+                <h1>Certificate here</h1>
+            </div>
+        );
+    }
+
+}
+export default SingleCertificate;
